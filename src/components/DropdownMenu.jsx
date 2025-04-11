3e063f4b363e3bace4f9f4878380a4dcd238f696
Wildes Sousa <augustowildes@gmail.com>
2025-04-11 15:24:23 -0300

    chore: Fixes #9 add action in menu btn

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -2,16 +2,16 @@ import { IoMdAddCircle } from "react-icons/io";
 import { BsCartCheckFill } from "react-icons/bs";
 import { LuListTodo } from "react-icons/lu";
 
-const DropdownMenu = () => {
+const DropdownMenu = ({setOpenPage}) => {
   return(
-    <section className="fixed bottom-0 rounded-2xl p-3 w-[50dvw] h-[9dvh] 
+    <section className="fixed bottom-0 rounded-3xl p-3 w-[50dvw] h-[10dvh] 
     flex bg-[#67918f] text-[#c8e9e5] mb-3 shadow-lg
       justify-between items-center text-center text-sm font-bold
      gap-3 " 
      style={{ display: "flex", flexDirection: "row" }}>
-      <LuListTodo onClick={()=> alert('Lista de compra')} className="w-15 h-5 hover:text-[#39ff14] transition-all duration-300 ease-in-out" />
-      <IoMdAddCircle  onClick={()=> alert('Adicionar produto')} className="text-[#39ff14] w-30 h-10  hover:text-[#72b8ad] transition-all duration-300 ease-in-out" />
-      <BsCartCheckFill onClick={()=> alert('Compra concluidas')} className=" w-15 h-5 hover:text-[#39ff14] transition-all duration-300 ease-in-out" />
+      <LuListTodo onClick={()=> setOpenPage('list')} className="md:w-30px w-15 h-5 hover:text-[#39ff14] transition-all duration-300 ease-in-out" />
+      <IoMdAddCircle  onClick={()=> setOpenPage('addProduct')} className="text-[#39ff14]  w-30 h-10  hover:text-[#72b8ad] transition-all duration-300 ease-in-out" />
+      <BsCartCheckFill onClick={()=> setOpenPage('cart')} className=" md:w-30px w-15 h-5 hover:text-[#39ff14] transition-all duration-300 ease-in-out" />
     </section>
   );
 }
