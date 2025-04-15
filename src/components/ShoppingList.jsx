1ab73a93bf90335ea3a6223e139a68ca68b3aeaa
Wildes Sousa <augustowildes@gmail.com>
2025-04-15 08:36:14 -0300

    :rocket: Fixes #3 componente criada

---
+++ src/components/ShoppingList.jsx
@@ -0,0 +1,15 @@
+import { IoIosArrowForward } from "react-icons/io";
+
+const ShoppingList = ({ item, onClick }) => {
+  const handleChange = () => {
+    onClick('list');
+  }
+  return (
+    <h5 key={item.id} className="flex justify-between items-center h-[25dvh] w-full bg-[#67918f] rounded-md p-2">
+      <span>{item.name}</span>
+      <IoIosArrowForward onClick={handleChange} className="w-[32px] h-[32px] bg-[#3b473e] rounded-2xl" />
+    </h5>
+  );
+}
+
+export default ShoppingList;
